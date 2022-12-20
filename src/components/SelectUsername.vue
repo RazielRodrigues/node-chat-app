2f451e6ccf37c77dc7289c10267f545cd8ac5e00
Raziel Rodrigues <razielx3@live.com>
2022-12-20 01:43:46 -0300

    att front end

--- src/components/SelectUsername.vue
+++ src/components/SelectUsername.vue
@@ -2,14 +2,19 @@
   <div class="">
 
     <div class="welcome">
-      <h1>CHAT PRIVADO BY RZ CORPS</h1>
+      <h1>Only Us 
+      <img class="welcome-logo" src="https://www.svgrepo.com/show/179277/chat-communication.svg" width="45px" height="30px"> </h1>
     </div>
 
     <div class="select-username">
       <form @submit.prevent="onSubmit">
-        <input v-model="username" placeholder="Your username..." />
-        <button :disabled="!isValid">Send</button>
+        <input v-model="username" placeholder="Seu nome no chat" />
+        <button :disabled="!isValid">Escolher nome</button>
       </form>
+    </div>
+
+    <div class="footer">
+      <span>made by Raziel Rodrigues</span>
     </div>
 
   </div>
@@ -41,10 +46,36 @@ export default {
 .welcome {
   text-align: center;
   margin-top: 5rem;
+  margin-bottom: 5rem;
+}
+
+.welcome-logo {
+  text-align: center;
+  margin-top: 4.5rem;
+  margin-bottom: 5rem;
 }
 
 .select-username {
-  width: 300px;
-  margin: 200px auto 0;
+  display: flex;
+}
+
+.select-username form {
+  margin: auto;
+  border:  #232323 2px solid;
+}
+
+.select-username button {
+  border:  #232323 2px solid;
+}
+
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #232323;
+   color: white;
+   text-align: center;
+   padding: 10px;
 }
 </style>
